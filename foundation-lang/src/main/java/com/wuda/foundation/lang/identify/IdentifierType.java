dc39603eba926e24b6811571d3281d64d893a70f
wuda0112 <wuda0112@gmail.com>
2020-07-18 19:53:36 +0800

    IdentifierType增加更多注释

--- foundation-lang/src/main/java/com/wuda/foundation/lang/identify/IdentifierType.java
+++ foundation-lang/src/main/java/com/wuda/foundation/lang/identify/IdentifierType.java
@@ -2,8 +2,11 @@ package com.wuda.foundation.lang.identify;
 
 /**
  * {@link Identifier}的类型.推荐用枚举实现该类.
+ * 系统已经内置了很多类型,在添加自定义的类型之前,可以先到
+ * {@link BuiltinIdentifierType}中查看是否已经存在.
  *
  * @author wuda
+ * @see BuiltinIdentifierType
  * @since 1.0.0
  */
 public interface IdentifierType {

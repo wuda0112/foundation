8728dc77a45ab940bdd50cc861257122c1a58828
wuda0112 <wuda0112@gmail.com>
2020-11-19 23:35:04 +0800

    permission合并功能完善

--- foundation-security/src/main/java/com/wuda/foundation/security/Subject.java
+++ foundation-security/src/main/java/com/wuda/foundation/security/Subject.java
@@ -21,4 +21,16 @@ public class Subject extends LongIdentifier {
     public Subject(Long value, IdentifierType type) {
         super(value, type);
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj instanceof Subject) {
+            Subject other = (Subject) obj;
+            return this.getType().getCode() == ((Subject) obj).getType().getCode() && this.getValue().equals(other.getValue());
+        }
+        return false;
+    }
 }
